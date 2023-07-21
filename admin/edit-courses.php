58809c4f1381fd615cb28d9fca2f721795d2d62f
himanshu9699 <himanshu3792.be21@chitkara.edu.in>
2023-07-21 20:32:54 +0530

    First

---
+++ admin/edit-courses.php
@@ -0,0 +1,164 @@
+<?php
+    session_start();
+    include('../includes/dbconn.php');
+    include('../includes/check-login.php');
+    check_login();
+
+    if(isset($_POST['submit'])){
+    $coursecode=$_POST['cc'];
+    $coursesn=$_POST['cns'];
+    $coursefn=$_POST['cnf'];
+    $id=$_GET['id'];
+    $query="UPDATE courses set course_code=?,course_sn=?,course_fn=? where id=?";
+    $stmt = $mysqli->prepare($query);
+    $rc=$stmt->bind_param('sssi',$coursecode,$coursesn,$coursefn,$id);
+    $stmt->execute();
+    echo"<script>alert('Selected course has been updated');
+    window.location.href='manage-courses.php';
+    </script>";
+    
+    }
+
+?>
+
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    <title>Hostel Management System</title>
+    <link href="../assets/extra-libs/c3/c3.min.css" rel="stylesheet">
+    <link href="../assets/libs/chartist/dist/chartist.min.css" rel="stylesheet">
+    <link href="../dist/css/style.min.css" rel="stylesheet">
+    
+</head>
+
+<body>
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
+    <div id="main-wrapper" data-theme="light" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed" data-boxed-layout="full">
+        <header class="topbar" data-navbarbg="skin6">
+            <?php include 'includes/navigation.php'?>
+        </header>
+        <aside class="left-sidebar" data-sidebarbg="skin6">
+            <div class="scroll-sidebar" data-sidebarbg="skin6">
+                <?php include 'includes/sidebar.php'?>
+            </div>
+        </aside>
+        <div class="page-wrapper">
+            <div class="page-breadcrumb">
+                <div class="row">
+                    <div class="col-7 align-self-center">
+                    <h4 class="page-title text-truncate text-dark font-weight-medium mb-1">Edit Courses</h4>
+                        <div class="d-flex align-items-center">
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <div class="container-fluid">
+
+                <form method="POST">
+
+                    <div class="row">
+
+
+                    <?php	
+						$id=$_GET['id'];
+                        $ret="SELECT * from courses where id=?";
+                            $stmt= $mysqli->prepare($ret) ;
+                        $stmt->bind_param('i',$id);
+                        $stmt->execute() ;
+                        $res=$stmt->get_result();
+                        
+                        while($row=$res->fetch_object())
+                        {
+                            ?>
+
+
+                        <div class="col-sm-12 col-md-6 col-lg-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Course Code</h4>
+                                        <div class="form-group">
+                                            <input type="text" name="cc" value="<?php echo $row->course_code;?>" id="cc" class="form-control" required>
+                                        </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+
+
+                        <div class="col-sm-12 col-md-6 col-lg-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Course Full Name</h4>
+                                        <div class="form-group">
+                                            <input type="text" name="cnf" value="<?php echo $row->course_fn;?>"  id="cnf" class="form-control" required>
+                                        </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-sm-12 col-md-6 col-lg-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Course Shortform</h4>
+                                        <div class="form-group">
+                                            <input type="text" name="cns" id="cns" value="<?php echo $row->course_sn;?>" required="required" class="form-control">
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <?php } ?>
+
+                    </div>
+                
+
+                        <div class="form-actions">
+                            <div class="text-center">
+                                <button type="submit" name="submit" class="btn btn-success">Update</button>
+                                <button type="reset" class="btn btn-danger">Reset</button>
+                            </div>
+                        </div>
+                
+                </form>
+
+
+            </div>
+            <?php include '../includes/footer.php' ?>
+        </div>
+    </div>
+    <script src="../assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="../assets/libs/popper.js/dist/umd/popper.min.js"></script>
+    <script src="../assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="../dist/js/app-style-switcher.js"></script>
+    <script src="../dist/js/feather.min.js"></script>
+    <script src="../assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+    <script src="../dist/js/sidebarmenu.js"></script>
+    <script src="../dist/js/custom.min.js"></script>
+    <script src="../assets/extra-libs/c3/d3.min.js"></script>
+    <script src="../assets/extra-libs/c3/c3.min.js"></script>
+    <script src="../assets/libs/chartist/dist/chartist.min.js"></script>
+    <script src="../assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
+    <script src="../dist/js/pages/dashboards/dashboard1.min.js"></script>
+
+</body>
+
+</html>
